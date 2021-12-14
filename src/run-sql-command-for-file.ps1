705d3d88e3ae2988611efbb601c5c471282977d0
joseschw <joseph.schwartz@extendhealth.com>
2021-12-14 07:41:52 -0700

    Fixing the create db step so it can run using integrated security

---
+++ src/run-sql-command-for-file.ps1
@@ -0,0 +1,34 @@
+param (
+    [string]$dbServer,
+    [string]$dbName,
+    [string]$pathToFile,
+    [string]$sqlCmdVariables,
+    [switch]$useIntegratedSecurity = $false,
+    [string]$username,
+    [securestring]$password
+)
+
+Import-Module SqlServer
+
+$sqlCmdParams = @(
+    "-InputFile $pathToFile"
+    "-ServerInstance `"$dbServer`""
+    "-Database `"master`""
+    "-AbortOnError"
+    "-SeverityLevel 0"
+    "-ErrorLevel 0"
+    "-Verbose"
+    "-Variable @(`"$sqlCmdVariables`")"
+)
+
+if (!$useIntegratedSecurity) {
+    $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username, $password
+    $plainPassword = $cred.GetNetworkCredential().Password
+
+    $sqlCmdParams += "-Username $username"
+    $sqlCmdParams += "-Password $plainPassword"
+}
+
+$paramsAsAString = [string]::Join(" ", $sqlCmdParams)
+
+Invoke-Expression -Command "Invoke-Sqlcmd $paramsAsAString"
