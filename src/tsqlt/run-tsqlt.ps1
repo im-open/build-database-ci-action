7ddaf482aef10a95354d1a1612aeb63c41902faf
Brian Belnap <Brian.Belnap@wtwco.com>
2024-08-26 09:55:36 -0600

    Comment out conversion of XML results to string

--- src/tsqlt/run-tsqlt.ps1
+++ src/tsqlt/run-tsqlt.ps1
@@ -45,7 +45,7 @@ if ($trustServerCertificate) {
 try {
     Write-Output "Executing tSQLt tests"
     Invoke-Expression "& sqlcmd $authParams -S `"$dbServer,$dbServerPort`" -d `"$dbName`" -Q `"$runTestsSql`" $queryTimeoutParam"
-    $results = Invoke-Expression "& sqlcmd $authParams -b -S `"$dbServer,$dbServerPort`" -d `"$dbName`" -h-1 -I -Q `"$getTestResultsSql`" $queryTimeoutParam" | ConvertTo-Xml -As String
+    $results = Invoke-Expression "& sqlcmd $authParams -b -S `"$dbServer,$dbServerPort`" -d `"$dbName`" -h-1 -I -Q `"$getTestResultsSql`" $queryTimeoutParam" #| ConvertTo-Xml -As String
 
     # Catch when an error happens in the test run (e.g. query timeout)
     if ($results -notlike "*testsuites*") {
